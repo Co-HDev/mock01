be40d80b69262ba7e5067233c2b984bdf38c31f9
Co-HDev <73033571+Co-HDev@users.noreply.github.com>
2020-11-06 13:56:25 -0600

    Moved styles to css file. Added jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
-// $( "a" ).click(function( event ) {
- 
-//     event.preventDefault();
- 
-//     $( this ).hide( "slow" );
- 
-// });
 
-// $('p').slideUp(8000);
-// $('p').slideDown(8000);
-
-$('img.slide').hide().toggle(500);
-// $('img').toggle(1000)
-// $('p').slideToggle(1800)
-// $('img').fadeIn(1050)
+$(document).ready(()=>{
+    $('img.hidden').slideToggle(400);
+});
 
