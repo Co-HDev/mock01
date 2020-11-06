548cf0644e163317c8446603376dd45674b94899
Co-HDev <73033571+Co-HDev@users.noreply.github.com>
2020-11-06 12:27:58 -0600

    Current Jeff mock project

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// $( "a" ).click(function( event ) {
+ 
+//     event.preventDefault();
+ 
+//     $( this ).hide( "slow" );
+ 
+// });
+
+// $('p').slideUp(8000);
+// $('p').slideDown(8000);
+
+$('img.slide').hide().toggle(500);
+// $('img').toggle(1000)
+// $('p').slideToggle(1800)
+// $('img').fadeIn(1050)
+
