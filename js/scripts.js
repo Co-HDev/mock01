d9e0be3e82397c0a91196061007fa47753845c67
Co-HDev <73033571+Co-HDev@users.noreply.github.com>
2020-11-07 02:26:57 -0600

    Even more design and UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 
 $(document).ready(()=>{
-    $('img.hidden').slideToggle(400);
+    $('img.hidden').delay(200).slideToggle(400);
 });
 
+// $('img.startup').slideToggle(800);
+
+// $( "img.startup" ).hide( 500 ).delay( 1500 ).show( 300 );
